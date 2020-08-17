2d84e7c85d8d69bfb3ec84a207d159969f06c28d
Vinicius Tesoni <vmtesoni@gmail.com>
2020-08-17 11:47:25 -0300

    Configurações do server e index

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+//CONSTS
+const PORT = 8081;
+const HOST = '0.0.0.1';
+
+//APP
+const app = express();
+app.use(express.static('public'))
+app.get('/',(res,res)=>{
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.listen(PORT,HOST);
+console.log('Running on http://${HOST}:${PORT}');
