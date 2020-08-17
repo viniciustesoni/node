06fab28e98b57da2146ec31391b7d2f9c6807391
Vinicius Tesoni <vmtesoni@gmail.com>
2020-08-17 12:28:12 -0300

    Correções no código

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const path = require("path");
 
 //CONSTS
 const PORT = 8081;
-const HOST = '0.0.0.1';
+const HOST = 'localhost';
 
 //APP
 const app = express();
 app.use(express.static('public'))
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(PORT,HOST);
-console.log('Running on http://${HOST}:${PORT}');
+console.log(`Running on http://${HOST}:${PORT}`);
