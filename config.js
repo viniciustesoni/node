ea4b670c8295c3d7c498b75b1230d0f5cad1e5aa
Vinicius Tesoni <vmtesoni@gmail.com>
2020-08-18 14:24:25 -0300

    teste 2 - Ajustes

--- config.js
+++ config.js
@@ -2,5 +2,9 @@ module.exports = {
   development: {
     port: process.env.PORT || 3000,
     saltingRounds: 10
+  },
+  production: {
+    port: process.env.PORT || 80,
+    saltingRounds: 10
   }
 }
