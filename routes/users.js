2ef6dd6267caa5d7f6431d7837c77fdfbb3cd646
Vinicius Tesoni <vmtesoni@gmail.com>
2020-08-20 17:54:18 -0300

    config test

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const validateToken = require('../utils').validateToken;
 module.exports = (router) => {
   router.route('/users')
     .post(controller.add)
-    .get(validateToken,controller.getAll); // This route will be protected shortly
+    .get(validateToken,controller.getAll);
 
   router.route('/login')
     .post(controller.login)
